583c3c8cd401260bd7ab3eebf9c9c3bdee286f43
ThomasDAP <95960414+ThomasDAP@users.noreply.github.com>
2022-02-25 17:06:14 +0100

    Changements
    Fin

--- td2-squelette/js/modules/product.js
+++ td2-squelette/js/modules/product.js
@@ -10,7 +10,10 @@ function Produit(référence, prix, description) {
 
 
 
-
+product.push(new Produit("#REF1", 250.29, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+product.push(new Produit("#REF2", 129.25, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+product.push(new Produit("#REF3", 18.10, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+product.push(new Produit("#REF4", 180.12, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
 product.push(new Produit("#PRT1", 22.0, "Produit de test"));
 product.push(new Produit("#PRT2", 40.0, "Autre produit de test"));
 
